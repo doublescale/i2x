0a0f04b53dcb790ec5186dd8605042a4e6880870
doublescale <doublescale@tutanota.com>
2023-10-03 00:58:55 +0200

    load multiple images

--- src/main.c
+++ src/main.c
@@ -37,11 +37,19 @@
 
 #define WINDOW_INIT_W 800
 #define WINDOW_INIT_H 600
-#define WINDOW_TITLE "i2x"
+#define PROGRAM_NAME "i2x"
 
 static const u32 replacement_character_codepoint = 0xFFFD;
 
 static void (*glXSwapIntervalEXT)(Display *dpy, GLXDrawable drawable, int interval);
+
+typedef struct
+{
+  GLuint texture_id;
+  i32 w;
+  i32 h;
+  u8* pixels;
+} img_entry_t;
 
 internal u64 get_nanoseconds()
 {
@@ -172,7 +180,7 @@ int main(int argc, char** argv)
         glXSwapIntervalEXT(display, glx_window, vsync);
 
         XChangeProperty(display, window, XA_WM_NAME, XA_STRING, 8, PropModeReplace,
-            (unsigned char*)WINDOW_TITLE, sizeof(WINDOW_TITLE) - 1);
+            (unsigned char*)PROGRAM_NAME, sizeof(PROGRAM_NAME) - 1);
         XMapWindow(display, window);
 
         glXMakeContextCurrent(display, glx_window, glx_window, glx_context);
@@ -187,30 +195,38 @@ int main(int argc, char** argv)
           XFreePixmap(display, empty_pixmap);
         }
 
-        i32 img_w = 0;
-        i32 img_h = 0;
-        u8* img_pixels = 0;
+        i32 img_count = max(1, argc - 1);
+        img_entry_t* img_entries = malloc_array(img_count, img_entry_t);
+        zero_bytes(img_count * sizeof(img_entry_t), img_entries);
+
         if(argc > 1)
         {
-          img_pixels = stbi_load(argv[1], &img_w, &img_h, 0, 4);
+          for(i32 img_idx = 0;
+              img_idx < img_count;
+              ++img_idx)
+          {
+            img_entry_t* img = &img_entries[img_idx];
+
+            img->pixels = stbi_load(argv[img_idx + 1], &img->w, &img->h, 0, 4);
 
 #if 0
-          // Premultiply alpha.
-          for(u64 i = 0; i < (u64)img_w * (u64)img_h; ++i)
-          {
-            if(img_pixels[4*i + 3] != 255)
+            // Premultiply alpha.
+            for(u64 i = 0; i < (u64)img->w * (u64)img->h; ++i)
             {
-              r32 r = img_pixels[4*i + 0] / 255.0f;
-              r32 g = img_pixels[4*i + 1] / 255.0f;
-              r32 b = img_pixels[4*i + 2] / 255.0f;
-              r32 a = img_pixels[4*i + 3] / 255.0f;
+              if(img->pixels[4*i + 3] != 255)
+              {
+                r32 r = img->pixels[4*i + 0] / 255.0f;
+                r32 g = img->pixels[4*i + 1] / 255.0f;
+                r32 b = img->pixels[4*i + 2] / 255.0f;
+                r32 a = img->pixels[4*i + 3] / 255.0f;
 
-              img_pixels[4*i + 0] = 255.0f * a * r;
-              img_pixels[4*i + 1] = 255.0f * a * g;
-              img_pixels[4*i + 2] = 255.0f * a * b;
+                img->pixels[4*i + 0] = 255.0f * a * r;
+                img->pixels[4*i + 1] = 255.0f * a * g;
+                img->pixels[4*i + 2] = 255.0f * a * b;
+              }
             }
-          }
 #endif
+          }
         }
 
         i32 win_w = WINDOW_INIT_W;
@@ -225,6 +241,8 @@ int main(int argc, char** argv)
         i64 nsecs_max = I64_MIN;
 
         b32 quitting = false;
+        i32 viewing_img_idx = 0;
+        i32 last_viewing_img_idx = -1;
         b32 border_sampling = true;
         b32 linear_sampling = true;
         b32 alpha_blend = true;
@@ -270,6 +288,16 @@ int main(int argc, char** argv)
                 if(keysym == XK_Escape)
                 {
                   quitting = true;
+                }
+                if(keysym == ' ')
+                {
+                  ++viewing_img_idx;
+                  if(viewing_img_idx >= img_count) { viewing_img_idx -= img_count; }
+                }
+                if(keysym == XK_BackSpace)
+                {
+                  --viewing_img_idx;
+                  if(viewing_img_idx < 0) { viewing_img_idx += img_count; }
                 }
                 else if(keysym == 'a')
                 {
@@ -341,6 +369,7 @@ int main(int argc, char** argv)
                 b32 went_down = (event.type == ButtonPress);
                 u32 button = event.xbutton.button;
                 b32 ctrl_held = (event.xbutton.state & 4);
+                b32 alt_held = (event.xbutton.state & 8);
                 r32 mouse_x = event.xbutton.x;
                 r32 mouse_y = win_h - event.xbutton.y - 1;
 
@@ -363,6 +392,11 @@ int main(int argc, char** argv)
                   {
                     zoom_delta = zoom_per_scroll;
                   }
+                  else if(alt_held)
+                  {
+                    --viewing_img_idx;
+                    if(viewing_img_idx < 0) { viewing_img_idx += img_count; }
+                  }
                   else
                   {
                     offset_y -= offset_per_scroll;
@@ -373,6 +407,11 @@ int main(int argc, char** argv)
                   if(ctrl_held)
                   {
                     zoom_delta = -zoom_per_scroll;
+                  }
+                  else if(alt_held)
+                  {
+                    ++viewing_img_idx;
+                    if(viewing_img_idx >= img_count) { viewing_img_idx -= img_count; }
                   }
                   else
                   {
@@ -493,6 +532,18 @@ int main(int argc, char** argv)
             }
           }
 
+          if(last_viewing_img_idx != viewing_img_idx)
+          {
+            char txt[256];
+            txt[0] = 0;
+            char* file_name = argv[viewing_img_idx + 1];
+            i32 txt_len = snprintf(txt, sizeof(txt), "%s - %s", PROGRAM_NAME, file_name);
+            XChangeProperty(display, window, XA_WM_NAME, XA_STRING, 8, PropModeReplace,
+                (unsigned char*)txt, txt_len);
+
+            last_viewing_img_idx = viewing_img_idx;
+          }
+
 #if 0
           if(game_memory.grab_mouse && !pointer_grabbed)
           {
@@ -553,11 +604,12 @@ int main(int argc, char** argv)
           glRotatef(30.0f, 0, 0, 1);
 #endif
 
-          if(!texture_id)
+          img_entry_t* img = &img_entries[viewing_img_idx];
+
+          if(!img->texture_id)
           {
             texture_needs_update = true;
-            glGenTextures(1, &texture_id);
-            glBindTexture(GL_TEXTURE_2D, texture_id);
+            glGenTextures(1, &img->texture_id);
 #if 0
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -566,6 +618,8 @@ int main(int argc, char** argv)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 #endif
           }
+
+          glBindTexture(GL_TEXTURE_2D, img->texture_id);
 
           u8 test_texels[] = {
             1, 1, 1, 255,
@@ -582,11 +636,11 @@ int main(int argc, char** argv)
           i32 tex_w = 3;
           i32 tex_h = 3;
 
-          if(img_pixels)
+          if(img->pixels)
           {
-            texels = img_pixels;
-            tex_w = img_w;
-            tex_h = img_h;
+            texels = img->pixels;
+            tex_w = img->w;
+            tex_h = img->h;
           }
 
           if(texture_needs_update)
